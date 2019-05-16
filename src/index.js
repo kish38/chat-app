f8a198ac28260c33e708537529edf705841769d1
Netala Kishore Kumar <softwaredeveloper@Netalas-MacBook-Air.local>
2019-05-16 13:07:27 +0530

    initial chat app setup

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+io.on('connection', (socket)=>{
+    console.log("New Websocket connection")
+
+    socket.emit("message", "Welcome!")
+    socket.broadcast.emit("message", "New User has Joined")
+
+    socket.on('sendMessage', (message) => {
+        io.emit("message", message)
+    })
+
+    socket.on('disconnect', () => {
+        io.emit("message", "User has left")
+    })
+})
+
+
+server.listen(port, ()=>{
+    console.log(`Server is up on port ${port}`)
+})
