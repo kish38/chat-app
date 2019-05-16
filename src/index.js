dbc9e067f13c61265930dbb8bc451433c30f79fb
kish38 <kishorekumarnetala@gmail.com>
2019-05-16 14:50:05 +0530

    moment and generateMessage

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
 const Filter = require('bad-words')
+const { generateMessage } = require('./utils/messages')
 
 const app = express()
 const server = http.createServer(app)
@@ -16,15 +17,16 @@ app.use(express.static(publicDirectoryPath))
 io.on('connection', (socket)=>{
     console.log("New Websocket connection")
 
-    socket.emit("message", "Welcome!")
-    socket.broadcast.emit("message", "New User has Joined")
+    socket.emit("message", generateMessage('Welcome!'))
+    socket.broadcast.emit("message", generateMessage("New User has Joined"))
 
     socket.on('sendMessage', (message, callback) => {
         const filter = new Filter()
         if (filter.isProfane(message)){
             return callback('Profanity is not allowed')
         }
-        io.emit("message", message)
+
+        io.emit("message", generateMessage(message))
         callback()
     })
 
@@ -34,7 +36,7 @@ io.on('connection', (socket)=>{
     })
 
     socket.on('disconnect', () => {
-        io.emit("message", "User has left")
+        io.emit("message", generateMessage("User has left"))
     })
 })
 
